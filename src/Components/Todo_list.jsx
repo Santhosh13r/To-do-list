3b177169e758cfc4f865ab81700a26869497ed23
Santhosh13r <162345493+Santhosh13r@users.noreply.github.com>
2024-12-03 17:27:30 +0530

    modify

--- src/Components/Todo_list.jsx
+++ src/Components/Todo_list.jsx
@@ -10,28 +10,28 @@ const Todo_list = () => {
 
     function AddTask() {
         if (Newtask.trim()) {
-            settask( [...Task ,Newtask]);
-            Setnewtask(' ');  
+            settask([...Task, Newtask]);
+            Setnewtask(' ');
         }
-      
+
 
 
     }
     function MoveDownTask(index) {
-        if (index < Task.length-1) {
-            const updatetask =[...Task];
-            [updatetask[index],updatetask[index + 1]]=[updatetask[index + 1], updatetask[index]];
+        if (index < Task.length - 1) {
+            const updatetask = [...Task];
+            [updatetask[index], updatetask[index + 1]] = [updatetask[index + 1], updatetask[index]];
             settask(updatetask);
-            
+
         }
 
     }
     function MoveUpTask(index) {
         if (index > 0) {
-            const updatetaskdown =[...Task];
-            [updatetaskdown[index],updatetaskdown[index - 1]]=[updatetaskdown[index - 1], updatetaskdown[index]];
+            const updatetaskdown = [...Task];
+            [updatetaskdown[index], updatetaskdown[index - 1]] = [updatetaskdown[index - 1], updatetaskdown[index]];
             settask(updatetaskdown);
-            
+
         }
 
 
@@ -42,16 +42,14 @@ const Todo_list = () => {
     }
 
     function DeleteTask(index) {
-        const update= Task.filter((_,i) => i !== index);
+        const update = Task.filter((_, i) => i !== index);
         settask(update);
-        
 
 
-    }
-
-    function completedTask() {
 
     }
+
+   
     return (
         <>
             <div className="container text-center mt-5 pt-5">
@@ -62,9 +60,9 @@ const Todo_list = () => {
                     <div className=" data col-12 ">
                         <div className="d-inline-flex gap-2">
                             <input type="text" placeholder='    Enter a Task ...' onChange={HandleInputChange} value={Newtask} />
-                            <button className='btn btn-outline-warning'  onClick={()=>AddTask()} >
+                            <button className='btn btn-outline-warning' onClick={() => AddTask()} >
                                 <i class="fa-solid fa-plus "></i>
-                                
+
                                 Add
                             </button>
 
@@ -75,11 +73,11 @@ const Todo_list = () => {
                                 <li key={index}>
                                     <span className='text'>{Task}</span>
 
-                                    <button className='btn btn-outline-warning m-2 ' onClick={()=>DeleteTask(index)}><i class="fa-solid fa-trash"></i>  </button> 
-                                    <button className='btn btn-outline-warning  m-2' onClick={()=>MoveUpTask(index)}><i class="fa-solid fa-arrow-up-long"></i></button>
-                                    <button className='btn btn-outline-warning m-2' onClick={()=>MoveDownTask(index)}><i class="fa-solid fa-angle-down"></i></button> 
+                                    <button className='btn btn-outline-warning m-2 ' onClick={() => DeleteTask(index)}><i class="fa-solid fa-trash"></i>  </button>
+                                    <button className='btn btn-outline-warning  m-2' onClick={() => MoveUpTask(index)}><i class="fa-solid fa-arrow-up-long"></i></button>
+                                    <button className='btn btn-outline-warning m-2' onClick={() => MoveDownTask(index)}><i class="fa-solid fa-angle-down"></i></button>
 
-                                    
+
                                 </li>
                             )}
                         </ol>
